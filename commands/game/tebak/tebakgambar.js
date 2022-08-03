0dc06c62fd5da35fb4f804b82fcf40d01d9d463c
HadatMTCH <hadatskywalker007@gmail.com>
2022-08-03 10:34:05 +0700

    Fix XP message

--- commands/game/tebak/tebakgambar.js
+++ commands/game/tebak/tebakgambar.js
@@ -23,7 +23,6 @@ module.exports = {
             let question = await msg.replyImage({ url: data.result.image }, 'Time 60 seconds!')
 
             _collection.set(msg.from, question)
-
             const collector = msg.createMessageCollector({
                 filter: data.result.answer,
                 max: 1,
@@ -31,13 +30,13 @@ module.exports = {
 
             collector.on('collect', (msg) => {
                 database.users.addExp(msg, msg.senderNumber, gainedXP)
-                msg.reply('Right! You received {xp} XP!'.format({ gainedXP }))
+                return msg.reply('Benar! Kamu mendapat {xp} XP!'.format({ xp: gainedXP }))
             });
 
             collector.on('end', (res) => {
                 _collection.delete(msg.from)
                 if (res == 'timeout') {
-                    msg.reply(`Timeout, answer is : ${data.result.answer}`, question)
+                    return msg.reply(`Waktu habis, jawabannya adalah : ${data.result.answer}`, question)
                 }
             });
             
