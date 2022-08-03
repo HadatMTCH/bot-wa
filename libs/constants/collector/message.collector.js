0da61dc5c4a42e479d1afb4a290126016dcfba17
HadatMTCH <hadatskywalker007@gmail.com>
2022-08-03 10:31:24 +0700

    Fix regex test on message collector

--- libs/constants/collector/message.collector.js
+++ libs/constants/collector/message.collector.js
@@ -49,22 +49,26 @@ class MessageCollector extends EventEmitter {
      * @returns
      */
     async messageHandler({ messages, type }) {
-        const message = messages[0]
-        if (type !== 'notify') return
-        if (message.key && message.key.remoteJid === 'status@broadcast') return
-        if (!message.message) return
-
-        const msg = await require('@libs/utils/serialize/serialize.util').serialize(message, this.client)
-        if (this.msg.from !== msg.from) return
-        if (typeof this.options.filter === 'string') {
-            this.options.filter = new RegExp(this.options.filter)
-        }
-        if (this.options.filter.test(msg.body.toLowerCase())) {
-            this.emit('collect', msg)
-            this.countMessage++
-            if (this.countMessage >= this.options.max) {
-                return this.stop('limit')
+        try {
+            const message = messages[0]
+            if (type !== 'notify') return
+            if (message.key && message.key.remoteJid === 'status@broadcast') return
+            if (!message.message) return
+    
+            const msg = await require('@libs/utils/serialize/serialize.util').serialize(message, this.client)
+            if (this.msg.from !== msg.from) return
+            if (typeof this.options.filter === 'string') {
+                this.options.filter = new RegExp(this.options.filter)
             }
+            if (this.options.filter.test(msg?.body?.toLowerCase())) {
+                this.emit('collect', msg)
+                this.countMessage++
+                if (this.countMessage >= this.options.max) {
+                    return this.stop('limit')
+                }
+            }
+        } catch (error) {
+            console.log("Error on collector handler: ", error);
         }
     }
 
